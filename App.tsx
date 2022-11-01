6b49c15e45f40f1bbd49166ed0c8f5c0a3b6b18e
cmrib <ciceromirandar@gmail.com>
2022-11-01 15:58:28 -0300

    config status bar

--- App.tsx
+++ App.tsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { NativeBaseProvider, Center, Text } from "native-base";
+import { NativeBaseProvider } from "native-base";
 import { THEME } from './src/styles/theme'
 import { Loading } from './src/components/Loading'
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { SignIn } from './src/screens/SignIn'
+import { StatusBar } from "native-base";
 
 
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
 
   return (
     <NativeBaseProvider theme={THEME}>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
 
       {!fontsLoaded ?
         <Loading />
