f49f060fd7a3b9ece2e0bf124c9b006bf492bb92
cmrib <ciceromirandar@gmail.com>
2022-11-03 12:31:52 -0300

    create SignIn screen

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,10 +1,28 @@
-import { Center, Text } from 'native-base'
+import { Center, Text, Icon } from 'native-base'
+import React from 'react'
+import Logo from '../assets/logo.svg'
+import { Button } from '../components/Button'
+import { Fontisto } from '@expo/vector-icons';
 
 export function SignIn() {
 
     return (
-        <Center flex={1} bgColor="gray.900" alignItems="center" justifyContent="center">
-            <Text color="yellow.500" fontSize={24}>Hello World</Text>
+        <Center flex={1} bgColor="gray.900" alignItems="center" justifyContent="center" p={7}>
+            <Logo width={212} height={40} />
+            <Button
+                title="entrar com google"
+                type="SECONDARY"
+                leftIcon={<Icon as={Fontisto} name="google" size="md" color="white" />}
+                mt={12}
+            />
+
+            <Text
+                color="white"
+                textAlign="center"
+                mt={4}
+            >
+                Não utilizamos nenhuma informação além {'\n'} do seu e-mail para criação de sua conta.
+            </Text>
         </Center>
     )
 }
